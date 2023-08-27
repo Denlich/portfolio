a13ed45e280ea1acc15c82c31e419316a50368e0
Denys Mykhailovych <68513347+Denlich@users.noreply.github.com>
2023-08-27 15:09:34 +0300

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -63,7 +63,7 @@ const Intro = () => {
       >
         <span className="font-bold">Hello, I'm Denys.</span> I'm a{" "}
         <span className="font-bold">fronted developer</span> with{" "}
-        <span className="font-bold">3 years</span> of experience. I enjoy
+        <span className="font-bold">1 year</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
